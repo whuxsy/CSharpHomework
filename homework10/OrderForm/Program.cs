99c4c5e2ce07a5f8dc3d20ac9f6e0e686e81e959
theoryxsy <2017302580167@whu.edu.com>
2018-11-29 21:20:02 +0800

    第十次提交

--- homework10/OrderForm/Program.cs
+++ homework10/OrderForm/Program.cs
@@ -14,11 +14,23 @@ namespace OrderForm
         [STAThread]
         static void Main()
         {
-           
+            try
+            {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new Form1());
-            
+            }
+            catch (Exception e)
+            {
+                if (MessageBox.Show(null, e.Message, "出错了", MessageBoxButtons.RetryCancel) == DialogResult.Cancel)
+                {
+                    return;
+                }
+                else
+                {
+                    Application.Run(new Form1());
+                }
+            }
         }
     }
 }
